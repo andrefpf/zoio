dff3b995c84a1dc22ff6d0b072fbacb6c3e3b290
André Fernandes <fpf.andre@gmail.com>
2022-05-07 15:03:42 -0300

    ADD: tests for dwt 2d

--- tests/test_dwt2d.cpp
+++ tests/test_dwt2d.cpp
@@ -26,34 +26,63 @@ void test_encode_dwt_2d() {
             TEST_CHECK(expected.at(i,j) == original.at(i,j));
         }
     }
-
 }
 
 void test_decode_dwt_2d() {
+    Matrix original = {
+        {1, 2, 1, 1},
+        {2, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+    };
 
+    Matrix expected = {
+        {1, 2, 3, 4},
+        {2, 3, 4, 5},
+        {3, 4, 5, 6},
+        {4, 5, 6, 7},
+    };
+
+    DWT2D::backward(original, 2);
+
+    for (int i = 0; i < expected.height(); i++) {
+        for (int j = 0; j < expected.width(); j++) {
+            TEST_CHECK(expected.at(i,j) == original.at(i,j));
+        }
+    }
 }
 
 void test_long_dwt_2d() {
+    int r;
+    int w = 1 << 12;
+    int h = 1 << 11;
 
+    srand(time(NULL));
+
+    Matrix original(w, h);
+    Matrix tmp(w, h);
+
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            r = rand() % 255;
+            original.at(i,j) = r;
+            tmp.at(i,j) = r;
+        }
+    }
+
+    DWT2D::foward(tmp, 5);
+    DWT2D::backward(tmp, 5);
+
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            TEST_CHECK(original.at(i,j) == tmp.at(i,j));
+        }
+    }
 }
 
 TEST_LIST = {
-    { "Encode DWT 2D",         test_encode_dwt_2d },
-    { "Dencode DWT 2D",        test_decode_dwt_2d },
-    { "Encode/Decode DWT 2D",    test_long_dwt_2d},
+    { "Encode DWT 2D",          test_encode_dwt_2d },
+    { "Decode DWT 2D",          test_decode_dwt_2d },
+    { "Encode/Decode DWT 2D",   test_long_dwt_2d},
     { NULL, NULL }
 };
-
-// {
-//     {1, 2, 3, 4},
-//     {2, 3, 4, 5},
-//     {3, 4, 5, 6},
-//     {4, 5, 6, 7},
-// }
-
-// {
-//     {1, 2, 1, 1},
-//     {2, 2, 1, 1},
-//     {1, 1, 1, 1},
-//     {1, 1, 1, 1},
-// }
